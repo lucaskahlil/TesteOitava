0b31ae432e89f7f41e2a1827920f74a5e0e52206
lucaskahlil <kahlilpereira@gmail.com>
2023-11-07 11:53:20 -0300

    style:  implement style CardTarefa

--- src/components/CardTarefa/CardTarefa.tsx
+++ src/components/CardTarefa/CardTarefa.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { useState } from 'react'
-import { TarefaPayload } from '../../utils/types/request'
+import React from "react";
+import { BtnEdit, CardItem, CheckContainer } from "./style";
+import { useState } from "react";
+import { TarefaPayload } from "../../utils/types/request";
 
 interface CardTarefaProps {
-  tarefa: TarefaPayload
+  tarefa: TarefaPayload;
 }
 
 export default function CardTarefa({ tarefa }: CardTarefaProps) {
   const [isCheck, setIsCheck] = useState(tarefa.done);
 
-
   return (
-    <div>
-      <input 
-        type="checkbox" 
-        checked={isCheck}
-        onChange={e => setIsCheck(e.target.checked)}
-      />
-      <h2>{tarefa.name}</h2>
-      <button>Editar</button>
-    </div>
-  )
+    <CardItem>
+      <CheckContainer>
+        <input 
+          type="checkbox"
+          checked={isCheck}
+          onChange={(e) => setIsCheck(e.target.checked)}
+        />
+        <label>{tarefa.name}</label>
+      </CheckContainer>
+      <BtnEdit>Editar</BtnEdit>
+    </CardItem>
+  );
 }
---
+++ src/components/CardTarefa/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const CardItem = styled.div`
+    display: flex;
+    justify-content: space-between  ;
+    margin-top: 15px;
+`
+
+export const CheckContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin-left: 20px;
+`
+
+export const BtnEdit = styled.button`
+    margin-right: 20px;
+`
+
+export const CheckBox = styled.input`
+    
+`
