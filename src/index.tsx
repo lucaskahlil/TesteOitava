f8717c972f3b2c6dc006763d81eee40192ed596b
Lucas Kahlil <kahlilpereira@gmail.com>
2023-11-07 21:49:15 -0300

    implement route in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/home/home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EditTask from './pages/EditTask/editTask';
 
 
 const root = ReactDOM.createRoot(
@@ -9,6 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    < Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={< Home />} />
+        <Route path='/edit/' element={<EditTask />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/EditTask/editTask.tsx
+++ src/pages/EditTask/editTask.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Task() {
+export default function EditTask() {
   return (
     <div>task</div>
   )
